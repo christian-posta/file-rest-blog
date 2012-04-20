12434a7208b187f69aac0e7a5a10f790e0a6220f
Christian Posta <christian.posta@gmail.com>
2012-04-20 16:55:34 -0700

    started adding supporting files

---
+++ src/main/java/com/fusesource/samples/CustomerServiceProcessor.java
@@ -0,0 +1,63 @@
+package com.fusesource.samples;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.apache.camel.component.cxf.common.message.CxfConstants;
+
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cposta
+ * Date: 4/20/12
+ * Time: 11:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CustomerServiceProcessor implements Processor {
+    private Map<Long, Customer> customers = new HashMap<Long, Customer>();
+    private Map<Long, Order> orders = new HashMap<Long, Order>();
+
+    public CustomerServiceProcessor() {
+        init();
+    }
+
+    final void init() {
+        Customer c = new Customer();
+        c.setName("John");
+        c.setId(123);
+        customers.put(c.getId(), c);
+
+        Order o = new Order();
+        o.setDescription("order 223");
+        o.setId(223);
+        orders.put(o.getId(), o);
+    }
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        Message inMessage = exchange.getIn();
+        String operationName = inMessage.getHeader(CxfConstants.OPERATION_NAME, String.class);
+        if ("getCustomer".equalsIgnoreCase(operationName)) {
+            String id = inMessage.getBody(String.class);
+            System.out.println("----invoking getCustomer, Customer id is: " + id);
+
+            long idNumber = Long.parseLong(id);
+
+            Customer c = customers.get(idNumber);
+
+            if (c == null) {
+                System.out.println("Customer is null: " + (c == null) );
+                Response response = Response.status(Response.Status.BAD_REQUEST).entity("<error>could not find customer</error>").build();
+                exchange.getOut().setBody(response);
+            }
+
+            exchange.getOut().setBody(c);
+
+
+        }
+
+    }
+}
